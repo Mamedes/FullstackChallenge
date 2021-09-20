a043319847635ab6f31773b9e054ebcbe067f7eb
Mamedes <mamedeser@gmail.com>
2021-09-20 18:45:40 -0400

    v_2_1 back end

--- backend/src/shared/infra/http/server/app.ts
+++ backend/src/shared/infra/http/server/app.ts
@@ -1,22 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable import/no-unresolved */
 import 'reflect-metadata';
+import 'dotenv/config';
+import cors from 'cors';
 import express from 'express';
 import 'express-async-errors';
-// import '../../../../scraping/index';
+import '../../../../scraping/index';
 import '../../../container/index';
+import '@shared/container';
 import createConnection from '../../typeorm/index';
 import { router } from '../routes/index.routes';
-
-import '@shared/container';
-
-import 'dotenv/config';
 
 createConnection();
 
 const app = express();
-
+console.log('app');
 app.use(express.json());
-
+app.use(cors());
 app.use(router);
 
 export { app };
